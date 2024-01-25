b5f79e6c73f4cd7fbb812399b25de253b36e81fa
rayner-69 <raynerang99@gmail.com>
2024-01-25 18:53:22 +0800

    cash on hand done

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,65 +1,68 @@
 from pathlib import Path 
 import csv 
-# create a file path to csv file.
+# Create a file path to csv file.
 fp = Path.cwd()/"csv_reports"/"Cash_on_Hand.csv"
 
-# read the csv file.
+# Read the csv file.
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
-    next(reader) # skip header
+    next(reader) # Skip header
 
-    # create an empty list
+    # Create an empty list
     Cash_on_Hand=[] 
 
-    # append day and cash on hand into the Cash_on_Hand list
+    # Append day and cash on hand into the Cash_on_Hand list
     for row in reader:
-        #get the day, cash on hand 
-        #and append to the Cash_on_Hand list
+        # Get the day, cash on hand 
+        # And append to the Cash_on_Hand list
         Cash_on_Hand.append([row[0],row[1]])   
 
-# print(Cash_on_Hand)
-
-
+# Create Lists for cash surplus, deficits and fluctuations
 surplus = []
 deficits = []
-flunctuate = []
-start = float(Cash_on_Hand[0][1])
+fluctuate = []
 
+start = float(Cash_on_Hand[0][1]) 
+
+# Calculate changes in cash on hand for each day
 for day in Cash_on_Hand[1:]: 
     
-    COH = float(day[1])
-    diff = COH - start 
+    COH = float(day[1]) # Make cash on hand a float
+    diff = round(COH - start) # Calculate change in cash on hand and round to 0dp
 
+    # Check for an increase in cash in hand/surplus
     if COH > start: 
         surplus.append((diff,day[0]))
-        flunctuate.append((diff,day[0]))
+        fluctuate.append((diff,day[0]))
         start = COH
 
+    # Check for an decrease in cash in hand/deficit
     elif COH < start: 
         deficits.append((diff,day[0]))
-        flunctuate.append((diff,day[0]))
+        fluctuate.append((diff,day[0]))
         start = COH 
 
-# print(surplus)
-# print(deficits)
-# print(flunctuate)
-
+# Prints results based on the scenarios
+# If cash on hand is always increasing
 if len(surplus) == len(Cash_on_Hand)-1: 
     surplus.sort()
-    print(f"[HIGHEST CASH SURPLUS] DAY: {surplus[-1][1]}, AMOUNT: {surplus[-1][0]}")
+    print(f"[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THEN THE PREVIOUS DAY")
+    print(f"[HIGHEST CASH SURPLUS] DAY: {surplus[-1][1]}, AMOUNT: SGD {abs(surplus[0][0])}")
 
+# If cash on hand is always decreasing
 elif len(deficits) == len(Cash_on_Hand)-1 : 
     deficits.sort()
-    print(f"[HIGHEST CASH DEFICIT] DAY: {deficits[0][1]}, AMOUNT: {deficits[0][0]}")
+    print(f"[CASH DEFICIT] CASH ON EACH DAY IS LOWER THEN THE PREVIOUS DAY")
+    print(f"[HIGHEST CASH DEFICIT] DAY: {deficits[0][1]}, AMOUNT: SGD {abs(deficits[0][0])}")
 
+# If cash on hand fluctuates
 else: 
-    for day in flunctuate: 
+    # Lists all days with a deficit and the amount
+    for day in fluctuate: 
         if day[0] < 0: 
-                print(f"[CASH DEFICIT] DAY: {day[1]}, AMOUNT: SGD{abs(day[0])}")
-    flunctuate.sort()
-    print(f"[HIGHEST CASH DEFICIT] DAY: {flunctuate[0][1]}, AMOUNT: SGD{abs(flunctuate[0][0])}")
-    print(f"[2ND HIGHEST CASH DEFICIT] DAY: {flunctuate[1][1]}, AMOUNT: SGD{abs(flunctuate[1][0])}")
-    print(f"[3RD HIGHEST CASH DEFICIT] DAY: {flunctuate[2][1]}, AMOUNT: SGD{abs(flunctuate[2][0])}")
-
-
-
+                print(f"[CASH DEFICIT] DAY: {day[1]}, AMOUNT: SGD {abs(day[0])}")
+    # Sorts and prints the top 3 highest deficits
+    fluctuate.sort()
+    print(f"[HIGHEST CASH DEFICIT] DAY: {fluctuate[0][1]}, AMOUNT: SGD {abs(fluctuate[0][0])}")
+    print(f"[2ND HIGHEST CASH DEFICIT] DAY: {fluctuate[1][1]}, AMOUNT: SGD {abs(fluctuate[1][0])}")
+    print(f"[3RD HIGHEST CASH DEFICIT] DAY: {fluctuate[2][1]}, AMOUNT: SGD {abs(fluctuate[2][0])}")
